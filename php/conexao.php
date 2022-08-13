bea1bad4a5d94149986b11442b0786c0d6af8a6c
Matheus Torres <108310425+048theuz@users.noreply.github.com>
2022-08-13 11:10:27 -0300

    conexão php vai ficar dentro da pasta database

--- php/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST', '127.0.0.1');
-define('usuario', 'root');
-define('senha', '');
-define('DB', 'login');
-
-$conexao = mysqli_connect('HOST', 'usuario', 'senha', 'DB') or die('Não foi possivel conectar');
